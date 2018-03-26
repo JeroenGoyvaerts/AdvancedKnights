82ec66ec2089c8f5e30ec3555ddd5af59a5ec1c9
jeroen goyvaerts <jeroen.goyvaerts1@hotmail.com>
2018-03-26 16:03:08 +0200

    Added Unit Class
    Added Unit Class to scripts folder

---
+++ AdvancedKnights/Advanced_Knights/Assets/Scripts/Unit.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit{
+
+    private string mUnitName = "Knight";
+    private int mUnitCost = 5;
+    private int mUnitHealth = 100;
+    private int mUnitAttackDamage = 15;
+
+    public string MUnitName
+    {
+        get
+        {
+            return mUnitName;
+        }
+
+        set
+        {
+            mUnitName = value;
+        }
+    }
+
+    public int MUnitCost
+    {
+        get
+        {
+            return mUnitCost;
+        }
+
+        set
+        {
+            mUnitCost = value;
+        }
+    }
+
+    public int MUnitHealth
+    {
+        get
+        {
+            return mUnitHealth;
+        }
+
+        set
+        {
+            mUnitHealth = value;
+        }
+    }
+
+    public int MUnitAttackDamage
+    {
+        get
+        {
+            return mUnitAttackDamage;
+        }
+
+        set
+        {
+            mUnitAttackDamage = value;
+        }
+    }
+
+    public Unit(string unitName, int unitCost, int unitHealth, int unitAttackDamage)
+    {
+        mUnitName = unitName;
+        mUnitCost = unitCost;
+        mUnitHealth = unitHealth;
+        mUnitAttackDamage = unitAttackDamage;
+    }
+
+    public Unit()
+    {
+
+    }
+
+}
