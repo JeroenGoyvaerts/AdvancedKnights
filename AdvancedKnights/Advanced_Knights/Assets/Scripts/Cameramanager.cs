b57e50eeea81a6dd0b913a196260ffaa7b671728
Stefan Beukema <36734201+powerzeus@users.noreply.github.com>
2018-03-27 12:00:32 +0200

    max min drag

--- AdvancedKnights/Advanced_Knights/Assets/Scripts/Cameramanager.cs
+++ AdvancedKnights/Advanced_Knights/Assets/Scripts/Cameramanager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Cameramanager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    float maxx = 15;
+    float maxy = -5;
+
+    float minx =  0;
+    float miny = -17;
 	// Update is called once per frame
 	void Update () {
         
@@ -21,6 +21,22 @@ public class Cameramanager : MonoBehaviour {
     {
         float xchange = this.transform.position.x + x;
         float ychange = this.transform.position.z + y;
+        if (xchange > maxx)
+        {
+            xchange = maxx;
+        }
+        else if (xchange < minx)
+        {
+            xchange = minx;
+        }
+        if (ychange > maxy)
+        {
+            ychange = maxy;
+        }
+        else if (ychange < miny)
+        {
+            ychange = miny;
+        }
         Vector3 newPosition = new Vector3(xchange, this.transform.position.y, ychange);
         this.transform.position = newPosition;
     }
