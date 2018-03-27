f7985093ac51046928639c9318960cf8e503cc3d
Stefan Beukema <36734201+powerzeus@users.noreply.github.com>
2018-03-27 16:00:27 +0200

    BuildingUI
    g

---
+++ AdvancedKnights/Advanced_Knights/Assets/BuildingUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingUI : MonoBehaviour {
+    public GameObject panel;
+
+    public void Activate()
+    {
+        panel.SetActive(true);
+    }   
+    public void DeActivate()
+    {
+        panel.SetActive(false);
+    }
+}
--- AdvancedKnights/Advanced_Knights/Assets/Scripts/Building.cs
+++ AdvancedKnights/Advanced_Knights/Assets/Scripts/Building.cs
@@ -5,32 +5,31 @@ using UnityEngine;
 public class Building : MonoBehaviour {
 
     public Player owner;
-    public Unit[] aUnit;
-    
+    GameObject BuildingUI;
+    private void Start()
+    {
+        BuildingUI = GameObject.Find("BuildingUI");
+    }
 
     public void Select()
     {
         MeshRenderer myrenderer = this.GetComponent<MeshRenderer>();
         myrenderer.material.color = new Color(1,0,0,1);
         Debug.Log("owner = " + owner.number);
+        if (owner == Gamemanager.Activeplayer)
+        {
+           BuildingUI.GetComponent<BuildingUI>().Activate();
+        }
+        
     }
     public void Deselect()
     {
         MeshRenderer myrenderer = this.GetComponent<MeshRenderer>();
         myrenderer.material.color = new Color(1, 1, 0, 1);
+
+        BuildingUI.GetComponent<BuildingUI>().DeActivate();
     }
-    public void Createunit(int unitNmb)
-    {
-        if(owner.gold - aUnit[unitNmb].MUnitCost >= 0)
-        {
-            owner.gold -= aUnit[unitNmb].MUnitCost;
-            Unit myUnit = Instantiate(aUnit[unitNmb]);
-            myUnit.owner = this.owner;
-            Vector3 buildingLocation = this.transform.position;
-            myUnit.transform.Translate(buildingLocation);
-            Debug.Log(owner.gold);
-        }
-    }
+    
 
 
 }
