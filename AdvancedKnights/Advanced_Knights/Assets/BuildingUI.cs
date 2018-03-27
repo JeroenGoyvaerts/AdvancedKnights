098e9c69049849969af598e09387e194ee9d0df9
Stefan Beukema <36734201+powerzeus@users.noreply.github.com>
2018-03-27 17:06:38 +0200

    fixes

--- AdvancedKnights/Advanced_Knights/Assets/BuildingUI.cs
+++ AdvancedKnights/Advanced_Knights/Assets/BuildingUI.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingUI : MonoBehaviour {
     public GameObject panel;
+    public Text playerText;
 
-    public void Activate()
+    public void Activate(string myText)
     {
         panel.SetActive(true);
+        SetText(myText);
     }   
     public void DeActivate()
     {
         panel.SetActive(false);
+    }
+    public void SetText(string myText)
+    {
+        playerText.text = myText;
     }
 }
