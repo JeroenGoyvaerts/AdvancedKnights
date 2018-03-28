b457b44cd4e70334ee213d4842623c497f67a681
jeroen goyvaerts <jeroen.goyvaerts1@hotmail.com>
2018-03-28 14:51:05 +0200

    test

---
+++ AdvancedKnights/Advanced_Knights/Assets/KnightScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightScript : MonoBehaviour
+{
+
+    public bool update;
+    public bool stateChangeable = true;
+
+    public Vector3 tilePosition;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        tilePosition = transform.position;
+        anim = GetComponent<Animator>();
+    }
+
+
+    void ChangeState(string state)
+    {
+        switch (state)
+        {
+            case "idle":
+                anim.SetInteger("state", -1);
+                break;
+
+            case "walk":
+                anim.SetInteger("state", 1);
+                break;
+
+            case "attack":
+                anim.SetInteger("state", 2);
+                break;
+
+            case "hurt":
+                anim.SetInteger("state", 3);
+                break;
+
+            case "die":
+                anim.SetInteger("state", 4);
+                break;
+
+            default:
+                anim.SetInteger("state", -1);
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacter();
+    }
+
+    public void UpdateCharacter()
+    {
+        Debug.Log((Vector3.Distance(transform.position, tilePosition)));
+        if (Vector3.Distance(transform.position, tilePosition) < 0.35)
+        {
+            ChangeState("idle");
+            stateChangeable = false;
+            if (transform.position == tilePosition + new Vector3(0, 0.15f, 0.3f))
+            {
+                update = false;
+            }
+        }
+        if (update)
+        {       
+            if (stateChangeable) ChangeState("walk");
+            transform.position = Vector3.Lerp(transform.position, tilePosition + new Vector3(0, 0.15f, 0.3f), Time.deltaTime * 3);
+        }
+    }
+
+    
+}
--- AdvancedKnights/Advanced_Knights/Assets/Scripts/LandTile.cs
+++ AdvancedKnights/Advanced_Knights/Assets/Scripts/LandTile.cs
@@ -8,6 +8,7 @@ public class LandTile : Selected {
     public Material selected;
 
     public GameObject SelectedUI;
+    public KnightScript knight;
 
     public string tileName = "Land";
     public string attributes = "none";
@@ -19,6 +20,12 @@ public class LandTile : Selected {
         myrenderer.sharedMaterial = selected;
 
         UpdateText(tileName,attributes);
+
+        knight = GameObject.Find("Knight").GetComponent<KnightScript>();
+        knight.tilePosition = myrenderer.transform.position;
+        knight.update = true;
+        knight.stateChangeable = true;
+        knight.UpdateCharacter();
     }
     public void Deselect()
     {
