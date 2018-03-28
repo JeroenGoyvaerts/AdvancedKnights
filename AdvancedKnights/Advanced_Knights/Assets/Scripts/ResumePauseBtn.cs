77377600b93df4febdfecb9a7efac2e9acac2a03
jeroen goyvaerts <jeroen.goyvaerts1@hotmail.com>
2018-03-28 12:23:56 +0200

    Added pause menu WiP

---
+++ AdvancedKnights/Advanced_Knights/Assets/Scripts/ResumePauseBtn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResumePauseBtn : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public GameObject gameManager;
+    public GameObject[] UIOnPauseDisabled;
+
+    public void pauseBtn()
+    {
+        pauseMenu.SetActive(true);
+        gameManager.SetActive(false);
+        Time.timeScale = 0;
+        foreach (GameObject UIElement in UIOnPauseDisabled)
+        {
+            UIElement.SetActive(false);
+        }
+    }
+
+    public void resumeBtn()
+    {
+        pauseMenu.SetActive(false);
+        gameManager.SetActive(true);
+        Time.timeScale = 1;
+        foreach (GameObject UIElement in UIOnPauseDisabled)
+        {
+            UIElement.SetActive(true);
+        }
+    }
+
+}
